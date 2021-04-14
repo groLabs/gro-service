1842bfae0b5413fe8b2777c5c23fa510c061f392
Wei Wang <wei@57blocks.com>
2021-04-14 09:17:35 +0800

    [ref] WW- 14.04.21 - PROT-729
       1. Fix duplicated factor entry issue
       2. Remove discord message in personal stats
       3. Refactor stats formatter

--- stats/handler/statsHandler.js
+++ stats/handler/statsHandler.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { BigNumber: BN } = require('bignumber.js');
+const mapObject = require('map-obj');
 const fs = require('fs');
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
@@ -43,99 +44,19 @@ const printUsd = function (value) {
     return BN(value.toString()).div(USD_DECIAML).toFixed(FIXED_USD).toString();
 };
 
-const formatOneAPY = function (originalAPY) {
-    return {
-        pwrd: printPercent(originalAPY.pwrd),
-        gvt: printPercent(originalAPY.gvt),
-    };
-};
-
-const formatStats = function (stats) {
-    const formatted = {
-        current_timestamp: stats.current_timestamp,
-        launch_timestamp: stats.launch_timestamp,
-        network: stats.network,
-    };
-    const originalApy = stats.apy;
-    formatted.apy = {
-        last24h: formatOneAPY(originalApy.last24h),
-        daily: formatOneAPY(originalApy.daily),
-        weekly: formatOneAPY(originalApy.weekly),
-        monthly: formatOneAPY(originalApy.monthly),
-        all_time: formatOneAPY(originalApy.all_time),
-    };
-    formatted.tvl = formatTvl(stats);
-    formatted.system = formatSystem(stats);
-    formatted.exposure = formatExposure(stats);
-    return formatted;
-};
-
-const formatTvl = function (stats) {
-    const originalTvl = stats.tvl;
-    const tvl = {
-        pwrd: printUsd(originalTvl.pwrd),
-        gvt: printUsd(originalTvl.gvt),
-        total: printUsd(originalTvl.total),
-        util_ratio: printPercent(originalTvl.util_ratio),
-        util_ratio_limit_PD: printPercent(originalTvl.util_ratio_limit_PD),
-        util_ratio_limit_GW: printPercent(originalTvl.util_ratio_limit_GW),
-    };
-    return tvl;
-};
-
-const formatVaults = function (vaults) {
-    return vaults.map((vault) => {
-        const strategies = vault.strategies.map((strategy) => {
-            return {
-                name: strategy.name,
-                amount: printUsd(strategy.amount),
-                share: printPercent(strategy.share),
-            };
-        });
-        return {
-            stablecoin: vault.stablecoin,
-            amount: printUsd(vault.amount),
-            share: printPercent(vault.share),
-            strategies,
-        };
-    });
-};
-
-const formatExposure = function (stats) {
-    const originalStablecoins = stats.exposure.stablecoins;
-    const originalProtocols = stats.exposure.protocols;
-    const stablecoins = originalStablecoins.map((item) => {
-        return {
-            stablecoin: item.stablecoin,
-            concentration: printPercent(item.concentration),
-        };
-    });
-    const protocols = originalProtocols.map((item) => {
-        return {
-            name: item.name,
-            concentration: printPercent(item.concentration),
-        };
-    });
-    return {
-        stablecoins,
-        protocols,
-    };
-};
-
-const formatSystem = function (stats) {
-    const originalSystem = stats.system;
-    const system = {
-        total_share: printPercent(originalSystem.total_share),
-        total_amount: printUsd(originalSystem.total_amount),
-        lifeguard: {
-            name: originalSystem.lifeguard.name,
-            amount: printUsd(originalSystem.lifeguard.amount),
-            share: printPercent(originalSystem.lifeguard.share),
+const mapper = function (original, percentKeys, amountKeys) {
+    return mapObject(
+        original,
+        (key, value) => {
+            if (percentKeys.length > 0 && percentKeys.includes(key)) {
+                return [key, printPercent(value)];
+            } else if (amountKeys.length > 0 && amountKeys.includes(key)) {
+                return [key, printUsd(value)];
+            }
+            return [key, value];
         },
-        vaults: formatVaults(originalSystem.vaults),
-    };
-
-    return system;
+        { deep: true }
+    );
 };
 
 const generateGroStatsFile = async function () {
@@ -149,19 +70,33 @@ const generateGroStatsFile = async function () {
         launch_timestamp: launchTimestamp,
         network: process.env.NODE_ENV.toLowerCase(),
     };
-    stats.apy = await getSystemApy(latestBlock);
-    stats.tvl = await getTvlStats(latestBlockTag);
-    stats.system = await getSystemStats(stats.tvl.total, latestBlockTag);
-    stats.exposure = await getExposureStats(latestBlockTag);
-    const formattedStats = formatStats(stats);
+    const apy = await getSystemApy(latestBlock);
+    stats.apy = mapper(apy, ['pwrd', 'gvt'], []);
+
+    const tvl = await getTvlStats(latestBlockTag);
+    stats.tvl = mapper(
+        tvl,
+        ['util_ratio', 'util_ratio_limit_PD', 'util_ratio_limit_GW'],
+        ['pwrd', 'gvt', 'total']
+    );
+
+    const system = await getSystemStats(tvl.total, latestBlockTag);
+    stats.system = mapper(
+        system,
+        ['total_share', 'share'],
+        ['total_amount', 'amount']
+    );
+    const exposure = await getExposureStats(latestBlockTag);
+    stats.exposure = mapper(exposure, ['concentration'], []);
+
     const statsFilename = `${statsDir}/gro-stats-${latestBlock.timestamp}.json`;
-    fs.writeFileSync(statsFilename, JSON.stringify(formattedStats));
+    fs.writeFileSync(statsFilename, JSON.stringify(stats));
     const latestFilename = {
         filename: statsFilename,
     };
     fs.writeFileSync(statsLatest, JSON.stringify(latestFilename));
     sendMessageToProtocolAssetChannel({
-        message: `\nPower Dollar:${formattedStats.tvl.pwrd}\nGro Vault:${formattedStats.tvl.gvt}\nTotalAssets:${formattedStats.tvl.total}\nUtilization Ratio:${formattedStats.tvl.util_ratio}\nUtilization Ratio Limit PD:${formattedStats.tvl.util_ratio_limit_PD}\nUtilization Ratio Limit GW:${formattedStats.tvl.util_ratio_limit_GW}`,
+        message: `\nPower Dollar:${formattedStats.tvl.pwrd}\nGro Vault:${formattedStats.tvl.gvt}\nTotalAssets:${formattedStats.tvl.total}\nUtilization Ratio:${formattedStats.tvl.util_ratio}`,
         type: MESSAGE_TYPES.stats,
     });
     return statsFilename;
