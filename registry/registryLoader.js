2a5f004a8aaf7a72dcb809bd4c97de6d38dafd3f
lily.hu <7158887-lilyhu2014@users.noreply.gitlab.com>
2021-07-07 15:35:52 +0800

    [feat] LH- 07.07.21- feature/PROT-1201   Add functions to load deployed contracts from registry

---
+++ registry/registryLoader.js
@@ -0,0 +1,46 @@
+const {
+    getLatestContracts,
+    getContractsHistory: getHistory,
+} = require('./registry');
+
+const botEnv = process.env.BOT_ENV.toLowerCase();
+// eslint-disable-next-line import/no-dynamic-require
+const logger = require(`../${botEnv}/${botEnv}Logger`);
+
+let latestContractsAddress;
+let latestContractsAddressByAddress;
+let contractsHistory;
+
+async function loadContractInfoFromRegistry() {
+    latestContractsAddress = await getLatestContracts(false);
+    latestContractsAddressByAddress = await getLatestContracts(true);
+    contractsHistory = await getHistory();
+    logger.info(
+        `latestContractsAddress: ${JSON.stringify(latestContractsAddress)}`
+    );
+    logger.info(
+        `latestContractsAddressByAddress: ${JSON.stringify(
+            latestContractsAddressByAddress
+        )}`
+    );
+    logger.info(`contractsHistory: ${JSON.stringify(contractsHistory)}`);
+}
+
+function getLatestContractsAddress() {
+    return latestContractsAddress;
+}
+
+function getLatestContractsAddressByAddress() {
+    return latestContractsAddressByAddress;
+}
+
+function getContractsHistory() {
+    return contractsHistory;
+}
+
+module.exports = {
+    loadContractInfoFromRegistry,
+    getLatestContractsAddress,
+    getLatestContractsAddressByAddress,
+    getContractsHistory,
+};
