6f0493df010a9e4b478c728bae26814d63b263ca
Sergi Juanati <wint3rmute@gmail.com>
2021-07-14 12:32:04 +0200

    Added API call to retrieve statsBot data

--- database/app.js
+++ database/app.js
@@ -1,16 +1,36 @@
 // const { loadGroStatsDB } = require('./loader/personalLoader');
 // const { loadGroStatsLiveDB } = require('./loader/personalLiveLoader');
 const { etlGroStats } = require('./etl/etlGroStats');
-const { groStatsHandler } = require('./handler/groStatsHandler');
+// const { groStatsHandler } = require('./handler/groStatsHandler');
+//const { groStatsCall } = require('./common/groStatsCall');
 
 
 //loadGroStatsDB();
 //loadGroStatsLiveDB('0xb5bE4d2510294d0BA77214F26F704d2956a99072');
-//etlGroStats();
-groStatsHandler();
+// etlGroStats();
+//groStatsHandler();
+
+(async () => {
+    try {
+        console.log('before etlGroStats()')
+        await etlGroStats();
+    } catch (e) {
+        console.log(e);
+    }
+})();
+
+// (async () => {
+//     try {
+//         var text = await groStatsCall();
+//         console.log(text);
+//     } catch (e) {
+//         console.log(e);
+//     }
+// })();
+
 
 
 /*
 npm i pg
 MODE_ENV=kovan BOT_ENV=STATS node ./stats_db/app.js
-*/ 
+*/
---
+++ database/queries/select/select_last_protocol_load.sql
@@ -0,0 +1,2 @@
+SELECT max(last_timestamp) as last_timestamp
+FROM gro."SYS_PROTOCOL_LOAD";
