f2a024a79f828cc0b684fb05050f40851eb4d8a0
Wei Wang <wei@57blocks.com>
2021-08-17 11:29:34 +0800

    [feat] WW- 16-08-21-feature/PROT-1769 (#70)
        1.Add strategy expected return check

--- stats/handler/systemHandler.js
+++ stats/handler/systemHandler.js
@@ -13,15 +13,27 @@ const {
     getStrategyLength,
     getVaultAndStrategyLabels,
     getBuoy,
+    getYearnVaults,
 } = require('../../contract/allContracts');
 const logger = require('../statsLogger');
-
+const { sendAlertMessage } = require('../../common/alertMessageSender');
 const { getCurrentApy } = require('./currentApyHandler');
-
+const {
+    MESSAGE_TYPES,
+    MESSAGE_EMOJI,
+    DISCORD_CHANNELS,
+    sendMessageToChannel,
+} = require('../../common/discord/discordService');
 // constant
 const SHARE_DECIMAL = BigNumber.from(10).pow(BigNumber.from(6));
 const THRESHOLD_DECIMAL = BigNumber.from(10).pow(BigNumber.from(4));
 const ZERO = BigNumber.from(0);
+const DECIMALS = [
+    BigNumber.from(10).pow(BigNumber.from(18)),
+    BigNumber.from(10).pow(BigNumber.from(6)),
+    BigNumber.from(10).pow(BigNumber.from(6)),
+    BigNumber.from(10).pow(BigNumber.from(18)),
+];
 
 // config
 const stabeCoinNames = config.get('stable_coin');
@@ -55,6 +67,86 @@ function calculateSharePercent(assets, total) {
 
 function convertToSharePercentDecimal(value) {
     return value.mul(SHARE_DECIMAL).div(THRESHOLD_DECIMAL);
+}
+
+async function checkStrategyChange(blockTag) {
+    try {
+        const vaults = getVaults();
+        const vaultAndStrategyLabels = getVaultAndStrategyLabels();
+        const yearnVaults = getYearnVaults(providerKey);
+        const emergencyThreshold = BigNumber.from(500);
+        const criticalThreshold = BigNumber.from(300);
+        const warningThreshold = BigNumber.from(150);
+        // not check curve strategy
+        for (
+            let vaultIndex = 0;
+            vaultIndex < vaults.length - 1;
+            vaultIndex += 1
+        ) {
+            const vaultLabel =
+                vaultAndStrategyLabels[vaults[vaultIndex].address];
+            const { strategies } = vaultLabel;
+            for (
+                let strategyIndex = 0;
+                strategyIndex < strategies.length;
+                strategyIndex += 1
+            ) {
+                const strat = strategies[strategyIndex].strategy;
+                const strategyName = strategies[strategyIndex].displayName;
+                const strategyStatus = await yearnVaults[vaultIndex].strategies(
+                    strat.address,
+                    blockTag
+                );
+                const totalAssets = strategyStatus.totalDebt;
+                const estimatedTotalAssets = await strat.estimatedTotalAssets(
+                    blockTag
+                );
+                const expectedReturn = estimatedTotalAssets.sub(totalAssets);
+                const msg = `Strategy expected return check vaultidx ${vaultIndex} stratidx ${strategyIndex} total ${totalAssets.div(
+                    DECIMALS[vaultIndex]
+                )} estimated ${estimatedTotalAssets.div(
+                    DECIMALS[vaultIndex]
+                )} expectedReturn ${expectedReturn.div(DECIMALS[vaultIndex])}`;
+                logger.info(msg);
+                if (
+                    expectedReturn.lt(BigNumber.from(0)) &&
+                    totalAssets.gt(BigNumber.from(0))
+                ) {
+                    const diff = expectedReturn
+                        .abs()
+                        .mul(SHARE_DECIMAL)
+                        .div(totalAssets);
+                    const expectedReturnUsd = expectedReturn.div(
+                        DECIMALS[vaultIndex]
+                    );
+                    const discordMessage = {
+                        message: msg,
+                        type: MESSAGE_TYPES.strategyAssets,
+                        description: '',
+                    };
+                    if (diff.gte(emergencyThreshold)) {
+                        discordMessage.description = `[EMERG] P2 - Strategy ${strategyName} expected return | Assets expected return is $${expectedReturnUsd} , The ratio is ${diff} BPS, threshold is ${emergencyThreshold} BPS (1/1000000)`;
+                        sendAlertMessage({ discord: discordMessage });
+                    } else if (diff.gte(criticalThreshold)) {
+                        discordMessage.description = `[CRIT] P2 - Strategy ${strategyName} expected return | Assets expected return is $${expectedReturnUsd} , The ratio is ${diff} BPS, threshold is ${criticalThreshold} BPS (1/1000000)`;
+                        sendAlertMessage({ discord: discordMessage });
+                    } else if (diff.gte(warningThreshold)) {
+                        discordMessage.description = `[WARN] P2 - Strategy ${strategyName} expected return | Assets expected return is $${expectedReturnUsd} , The ratio is ${diff} BPS, threshold is ${warningThreshold} BPS (1/1000000)`;
+                        sendAlertMessage({ discord: discordMessage });
+                    }
+                    sendMessageToChannel(
+                        DISCORD_CHANNELS.botLogs,
+                        discordMessage
+                    );
+                    logger.info(
+                        `totalAssets check ${discordMessage.description}`
+                    );
+                }
+            }
+        }
+    } catch (e) {
+        logger.error(e);
+    }
 }
 
 async function getCurveStrategyStats(vault, vaultTotalAsset, blockTag) {
@@ -94,71 +186,6 @@ async function getCurveVaultStats(blockTag) {
         amount: assetUsd,
         strategies: strategyStats,
     };
-}
-
-async function getStrategiesStatsOld(
-    vault,
-    index,
-    length,
-    vaultTotalAsset,
-    blockTag
-) {
-    const strategies = [];
-    let reservedAssets = BigNumber.from(vaultTotalAsset);
-    logger.info(`vta ${vaultTotalAsset} ra ${reservedAssets}`);
-    for (let j = 0; j < length; j += 1) {
-        const strategyAssets = await vault.getStrategyAssets(j, blockTag);
-        reservedAssets = reservedAssets.sub(strategyAssets);
-        const strategyAssetsUsd = await getUsdValue(
-            index,
-            strategyAssets,
-            blockTag
-        );
-        strategies.push({
-            name: strategyNames[index * 2 + j],
-            amount: strategyAssetsUsd,
-            assets: strategyAssets,
-        });
-    }
-    const reservedUSD = await getUsdValue(index, reservedAssets, blockTag);
-    strategies.push({
-        name: stabeCoinNames[index],
-        amount: reservedUSD,
-        assets: reservedAssets,
-    });
-    return strategies;
-}
-
-async function getVaultStatsOld(blockTag) {
-    const vaults = getVaults(providerKey);
-    const strategyLength = getStrategyLength();
-    const vaultAssets = [];
-    //
-    for (let vaultIndex = 0; vaultIndex < vaults.length - 1; vaultIndex += 1) {
-        const vault = vaults[vaultIndex];
-        const vaultTotalAsset = await vault.totalAssets(blockTag);
-        const assetUsd = await getUsdValue(
-            vaultIndex,
-            vaultTotalAsset,
-            blockTag
-        );
-        const strategyStats = await getStrategiesStatsOld(
-            vault,
-            vaultIndex,
-            strategyLength[vaultIndex],
-            vaultTotalAsset,
-            blockTag
-        );
-        vaultAssets.push({
-            name: vaultNames[vaultIndex],
-            amount: assetUsd,
-            strategies: strategyStats,
-        });
-    }
-    const curveVaultStats = await getCurveVaultStats(blockTag);
-    vaultAssets.push(curveVaultStats);
-
-    return vaultAssets;
 }
 
 async function getStrategiesStats(
@@ -248,11 +275,11 @@ async function getExposureStats(blockTag, systemStats) {
     }));
     const exposureProtocol = [];
     const protocols = [];
-    const vaultsStats = systemStats.vaults;
+    const vaultsStats = systemStats.vault;
     for (let i = 0; i < vaultsStats.length; i += 1) {
         const vault = vaultsStats[i];
         const strategyList = vault.strategies;
-        for (let j = 0; j < strategyList.length - 1; j += 1) {
+        for (let j = 0; j < strategyList.length; j += 1) {
             const propertyIndex = i * 2 + j;
             const strategy = strategyList[j];
             const strategyProtocols = strategyExposure[propertyIndex];
@@ -319,6 +346,8 @@ async function getTvlStats(blockTag) {
 }
 
 async function getSystemStats(totalAssetsUsd, blockTag) {
+    logger.info('check strategy expected return');
+    await checkStrategyChange(blockTag);
     logger.info('SystemStats');
     const vaults = getVaults();
     const lifeGuardStats = await getLifeguardStats(blockTag);
@@ -392,59 +421,12 @@ async function getSystemStats(totalAssetsUsd, blockTag) {
             strategies,
         };
     });
-    const vaultAssetsOld = await getVaultStatsOld(blockTag);
-    const oldVaultStats = vaultAssetsOld.map((vaultAsset, vaultIndex) => {
-        const vaultStrategyApy = currentApy[vaults[vaultIndex].address];
-        let vaultApy = BigNumber.from(0);
-        let vaultPercent = BigNumber.from(0);
-        const strategies = vaultAsset.strategies.map(
-            (strategy, strategyIndex) => {
-                const strat = vaultStrategyApy.strategies[strategyIndex];
-                let stratApy = BigNumber.from(0);
-                if (strat !== undefined) {
-                    logger.info(`strat apy ${strat.address} ${strat.apy}`);
-                    stratApy = strat.apy;
-                }
-                const strategyPercent = calculateSharePercent(
-                    strategy.amount,
-                    totalAssetsUsd
-                );
-                logger.info(
-                    `strat address ${strategyIndex} ${stratApy} ${strategyPercent}`
-                );
-                vaultApy = vaultApy.add(stratApy.mul(strategyPercent));
-                vaultPercent = vaultPercent.add(strategyPercent);
-                return {
-                    name: strategy.name,
-                    amount: strategy.amount,
-                    last3d_apy: stratApy,
-                    share: strategyPercent,
-                };
-            }
-        );
-        let estimatedVaultApy;
-        if (vaultPercent.isZero()) {
-            estimatedVaultApy = ZERO;
-        } else {
-            estimatedVaultApy = vaultApy.div(vaultPercent);
-        }
-        logger.info(`estimatedVaultApy ${vaultIndex} ${estimatedVaultApy}`);
-        const share = calculateSharePercent(vaultAsset.amount, totalAssetsUsd);
-        return {
-            name: vaultAsset.name,
-            amount: vaultAsset.amount,
-            share,
-            last3d_apy: estimatedVaultApy,
-            strategies,
-        };
-    });
     const systemStats = {
         total_share: systemShare,
         total_amount: systemTotal,
         last3d_apy: systemApy.div(SHARE_DECIMAL),
         lifeguard: lifeGuardStats,
         vault: vaultStats,
-        vaults: oldVaultStats,
     };
     return systemStats;
 }
