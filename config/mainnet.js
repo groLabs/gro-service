e40541a337d3987f222abc7cc6dc4b28368b32c5
Sergi Juanati <wint3rmute@gmail.com>
2021-09-17 10:36:19 +0200

    LBP mainnet config update

--- config/mainnet.js
+++ config/mainnet.js
@@ -230,4 +230,23 @@ module.exports = {
             port: 443,
         },
     },
+    lbp: {
+        // V2 aKlima
+        start_block: 27191741,
+        start_timestamp: 1631589784,
+        lbp_start_date: 1631631600,
+        lbp_end_date: 1631890800,
+        lbp_gro_start_weight: 0.96,
+        lbp_gro_end_weight: 0.1,
+        gro_amount_total: 120000,
+        usdc_amount_total: 579500,
+        balancerV2_graph_url: 'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-v2',
+        balancerV2_pool_address: '0x6Aa8A7B23F7B3875a966dDCc83D5b675cC9af54B',
+        balancerV2_pool_id: '0x6aa8a7b23f7b3875a966ddcc83d5b675cc9af54b00020000000000000000008e',
+        // V1
+        gro_token: '0xD348b1F5872940901fcAF9aCD1b9785f4e12121A',
+        coin_token: '0x9edF2989C22C5bF6675a4581E39D75a3C9BF8578',
+        bp_pool: '0x86fa758a31ba14d1beea4a79eece71c2329d004b',
+        crp_pool: '0xade0c1536e2053328b53a7b41b32289bce2cf65d',
+    },
 };
