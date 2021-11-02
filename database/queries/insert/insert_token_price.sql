c8b762e93ed4e68284c4aedeecc76ae4e7bebb25
Sergi Juanati <wint3rmute@gmail.com>
2021-11-02 19:17:50 +0100

    Pricing for GRO token from Coingecko

---
+++ database/queries/insert/insert_token_price.sql
@@ -0,0 +1,16 @@
+INSERT INTO gro."TOKEN_PRICE" (
+        "price_date",
+        "network_id",
+        "gvt_value",
+        "pwrd_value",
+        "gro_value",
+        "creation_date"
+    )
+VALUES (
+        $1,
+        $2,
+        $3,
+        $4,
+        $5,
+        $6
+    )
