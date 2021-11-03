2be21e9148f6d244965e632c0715a12dd2328910
Sergi Juanati <wint3rmute@gmail.com>
2021-11-03 13:54:02 +0100

    Recursive call to TokenCounter SC

--- database/constants.js
+++ database/constants.js
@@ -1,6 +1,7 @@
 const QUERY_ERROR = 400;
 const QUERY_SUCCESS = 200;
 const ERC20_TRANSFER_SIGNATURE = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
+const BALANCES_BATCH = 750;
 
 // Restrict eligible tables to extract data when called from API
 const TABLE_WHITELIST = [
@@ -14,4 +15,5 @@ module.exports = {
     QUERY_SUCCESS,
     ERC20_TRANSFER_SIGNATURE,
     TABLE_WHITELIST,
+    BALANCES_BATCH,
 }
--- database/loader/loadUserBalances2.js
+++ database/loader/loadUserBalances2.js
@@ -23,6 +23,7 @@ const {
     getBalancesUniBalLP,
     getBalancesCrvLP,
 } = require('../common/balanceUtil');
+const { BALANCES_BATCH: BATCH } = require('../constants');
 
 
 const { getConfig } = require('../../common/configUtil');
@@ -31,6 +32,75 @@ const GRO_GVT_ADDRESS = getConfig('staker_pools.contracts.uniswap_gro_gvt_pool_a
 const GRO_USDC_ADDRESS = getConfig('staker_pools.contracts.uniswap_gro_usdc_pool_address');
 const CRV_PWRD_ADDRESS = getConfig('staker_pools.contracts.curve_pwrd3crv_pool_address');
 const GRO_WETH_ADDRESS = getConfig('staker_pools.contracts.balancer_gro_weth_pool_address');
+
+
+const getBalancesSC = async (users, block, offset, gvt, pwrd, gro, lpGroGvt, lpGroUsdc, lpCrvPwrd, lpGroWeth) => {
+    try {
+
+        const newOffset = (offset + BATCH >= users.length)
+            ? users.length
+            : offset + BATCH;
+
+        const userBatch = users.slice(offset, newOffset);
+
+        logger.info(`**DB: Reading balances from TokenCounter() for users ${offset} to ${newOffset}...`);
+
+        const [
+            gvtUpdate,
+            pwrdUpdate,
+            groUpdate,
+            lpGroGvtUpdate,
+            lpGroUsdcUpdate,
+            lpCrvPwrdUpdate,
+            lpGroWethUpdate,
+        ] = await Promise.all([
+            getBalances(getGroVault().address, userBatch, block),
+            getBalances(getPowerD().address, userBatch, block),
+            getBalances(GRO_ADDRESS, userBatch, block),
+            getBalancesUniBalLP(GRO_GVT_ADDRESS, userBatch, block),
+            getBalancesUniBalLP(GRO_USDC_ADDRESS, userBatch, block),
+            getBalancesCrvLP(CRV_PWRD_ADDRESS, userBatch, block),
+            getBalancesUniBalLP(GRO_WETH_ADDRESS, userBatch, block),
+        ]);
+
+        if (gvt.length === 0) {
+            gvt = gvtUpdate;
+            pwrd = pwrdUpdate;
+            gro = groUpdate;
+            lpGroGvt = lpGroGvtUpdate;
+            lpGroUsdc = lpGroUsdcUpdate;
+            lpCrvPwrd = lpCrvPwrdUpdate;
+            lpGroWeth = lpGroWethUpdate;
+        } else {
+            gvt[0].amount_unstaked.push(...gvtUpdate[0].amount_unstaked);
+            gvt[1].amount_staked.push(...gvtUpdate[1].amount_staked);
+            pwrd[0].amount_unstaked.push(...pwrdUpdate[0].amount_unstaked);
+            pwrd[1].amount_staked.push(...pwrdUpdate[1].amount_staked);
+            gro[0].amount_unstaked.push(...groUpdate[0].amount_unstaked);
+            gro[1].amount_staked.push(...groUpdate[1].amount_staked);
+            lpGroGvt[0].amount_unstaked_lp.push(...lpGroGvtUpdate[0].amount_unstaked_lp);
+            lpGroGvt[1].amount_staked_lp.push(...lpGroGvtUpdate[1].amount_staked_lp);
+            lpGroGvt[2].lp_position.push(...lpGroGvtUpdate[2].lp_position);
+            lpGroUsdc[0].amount_unstaked_lp.push(...lpGroUsdcUpdate[0].amount_unstaked_lp);
+            lpGroUsdc[1].amount_staked_lp.push(...lpGroUsdcUpdate[1].amount_staked_lp);
+            lpGroUsdc[2].lp_position.push(...lpGroUsdcUpdate[2].lp_position);
+            lpCrvPwrd[0].amount_unstaked_lp.push(...lpCrvPwrdUpdate[0].amount_unstaked_lp);
+            lpCrvPwrd[1].amount_staked_lp.push(...lpCrvPwrdUpdate[1].amount_staked_lp);
+            lpCrvPwrd[2].lp_position.push(...lpCrvPwrdUpdate[2].lp_position);
+            lpGroWeth[0].amount_unstaked_lp.push(...lpGroWethUpdate[0].amount_unstaked_lp);
+            lpGroWeth[1].amount_staked_lp.push(...lpGroWethUpdate[1].amount_staked_lp);
+            lpGroWeth[2].lp_position.push(...lpGroWethUpdate[2].lp_position);
+        }
+
+        return (newOffset >= users.length)
+            ? [gvt, pwrd, gro, lpGroGvt, lpGroUsdc, lpCrvPwrd, lpGroWeth]
+            : getBalancesSC(users, block, newOffset, gvt, pwrd, gro, lpGroGvt, lpGroUsdc, lpCrvPwrd, lpGroWeth);
+
+    } catch (err) {
+        handleErr(`loadUserBalances2->getBalancesSC()`, err);
+        // return [] ?
+    }
+}
 
 /// @notice Load balances into USER_STD_FACT_BALANCES
 /// @dev    - Data is sourced from SC calls to users' balances at a certain block number
@@ -68,7 +138,7 @@ const loadUserBalances2 = async (
         // For each date, check each gvt & pwrd balance and insert data into USER_STD_FACT_BALANCES
         // TODO: *** select transfers where date < target date (for future data reloads)
         const dates = generateDateRange(fromDate, toDate);
-        logger.info(`**DB${account ? ' CACHE' : ''}: Processing ${res.rowCount} user balance${isPlural(res.rowCount)}...`);
+        logger.info(`**DB${account ? ' CACHE' : ''}: Processing ${users.length} user balance${isPlural(users.length)}...`);
 
         for (const date of dates) {
 
@@ -84,48 +154,37 @@ const loadUserBalances2 = async (
             let rowCountPooled = 0;
             let rowExcludedPooled = 0;
 
-            const GVT_ADDRESS = getGroVault().address;
-            const PWRD_ADDRESS = getPowerD().address;
             const block = (await findBlockByDate(day, false)).block;
 
-            logger.info(`**DB: Checking balances in TokenCounter() for ${users.length} users...`);
+            // logger.info(`**DB: Checking balances in TokenCounter() for ${users.length} users...`);
 
-            // TODO: perhaps make it recursive and build the arrays in buckets of 500?
             const [
-                gvtValue,
-                pwrdValue,
-                groValue,
+                gvt,
+                pwrd,
+                gro,
                 lpGroGvt,
                 lpGroUsdc,
                 lpCrvPwrd,
-                lpGroWeth,
-            ] = await Promise.all([
-                getBalances(GVT_ADDRESS, users, block),
-                getBalances(PWRD_ADDRESS, users, block),
-                getBalances(GRO_ADDRESS, users, block),
-                getBalancesUniBalLP(GRO_GVT_ADDRESS, users, block),
-                getBalancesUniBalLP(GRO_USDC_ADDRESS, users, block),
-                getBalancesCrvLP(CRV_PWRD_ADDRESS, users, block),
-                getBalancesUniBalLP(GRO_WETH_ADDRESS, users, block),
-            ]);
+                lpGroWeth
+            ] = await getBalancesSC(users, block, 0, [], [], [], [], [], [], []);
 
             for (let i = 0; i < users.length; i++) {
 
                 const addr = users[i];
 
                 const isUnstakedBalance = (
-                    gvtValue[0].amount_unstaked[i]
-                    + pwrdValue[0].amount_unstaked[i]
-                    + groValue[0].amount_unstaked[i]
+                    gvt[0].amount_unstaked[i]
+                    + pwrd[0].amount_unstaked[i]
+                    + gro[0].amount_unstaked[i]
                     > 0)
                     ? true
                     : false;
 
                 const isStakedBalance = (
-                    groValue[1].amount_staked[i]
+                    gro[1].amount_staked[i]
                     + lpGroGvt[1].amount_staked_lp[i]
                     + lpGroUsdc[1].amount_staked_lp[i]
-                    + gvtValue[1].amount_staked[i]
+                    + gvt[1].amount_staked[i]
                     + lpCrvPwrd[1].amount_staked_lp[i]
                     + lpGroWeth[1].amount_staked_lp[i]
                     > 0)
@@ -139,7 +198,7 @@ const loadUserBalances2 = async (
                     lpGroUsdc[0].amount_unstaked_lp[i] +
                     lpGroUsdc[2].lp_position[i][0] +
                     lpGroUsdc[2].lp_position[i][1] +
-                    lpCrvPwrd[0].amount_unstaked_lp[0] +
+                    lpCrvPwrd[0].amount_unstaked_lp[i] +
                     lpCrvPwrd[2].lp_position[i] +
                     lpGroWeth[0].amount_unstaked_lp[i] +
                     lpGroWeth[2].lp_position[i][0] +
@@ -152,9 +211,9 @@ const loadUserBalances2 = async (
                     day,
                     getNetworkId(),
                     addr,
-                    gvtValue[0].amount_unstaked[i],     // unstaked gvt amount
-                    pwrdValue[0].amount_unstaked[i],    // unstaked pwrd amount
-                    groValue[0].amount_unstaked[i],     // unstaked gro amount
+                    gvt[0].amount_unstaked[i],     // unstaked gvt amount
+                    pwrd[0].amount_unstaked[i],    // unstaked pwrd amount
+                    gro[0].amount_unstaked[i],     // unstaked gro amount
                     moment.utc(),
                 ];
 
@@ -162,10 +221,10 @@ const loadUserBalances2 = async (
                     day,
                     getNetworkId(),
                     addr,
-                    groValue[1].amount_staked[i],       // pool0_staked_amount
+                    gro[1].amount_staked[i],            // pool0_staked_amount
                     lpGroGvt[1].amount_staked_lp[i],    // pool1_staked_amount
                     lpGroUsdc[1].amount_staked_lp[i],   // pool2_staked_amount
-                    gvtValue[1].amount_staked[i],       // pool3_staked_amount
+                    gvt[1].amount_staked[i],            // pool3_staked_amount
                     lpCrvPwrd[1].amount_staked_lp[i],   // pool4_staked_amount
                     lpGroWeth[1].amount_staked_lp[i],   // pool5_staked_amount
                     moment.utc(),
@@ -181,7 +240,7 @@ const loadUserBalances2 = async (
                     lpGroUsdc[0].amount_unstaked_lp[i], // pool2_lp_amount
                     lpGroUsdc[2].lp_position[i][0],     // pool2_gro_amount
                     lpGroUsdc[2].lp_position[i][1],     // pool2_usdc_amount
-                    lpCrvPwrd[0].amount_unstaked_lp[0], // pool4_lp_amount
+                    lpCrvPwrd[0].amount_unstaked_lp[i], // pool4_lp_amount
                     lpCrvPwrd[2].lp_position[i],        // poll4_pwrd_amount
                     lpGroWeth[0].amount_unstaked_lp[i], // pool5_lp_amount
                     lpGroWeth[2].lp_position[i][0],     // pool5_gro_amount
@@ -233,6 +292,7 @@ const loadUserBalances2 = async (
             showMsg(account, date, rowCountStaked, rowExcludedStaked, 'USER_STD_FACT_BALANCES_STAKED');
             showMsg(account, date, rowCountPooled, rowExcludedPooled, 'USER_STD_FACT_BALANCES_POOLED');
         }
+
         // Update table SYS_USER_LOADS with the last loads
         if (account) {
             return true;
@@ -240,8 +300,9 @@ const loadUserBalances2 = async (
             const res = await loadTableUpdates('USER_STD_FACT_BALANCES', fromDate, toDate);
             return (res) ? true : false;
         }
+
     } catch (err) {
-        handleErr(`loadUserBalances->loadUserBalances() [from: ${fromDate}, to: ${toDate}]`, err);
+        handleErr(`loadUserBalances2->loadUserBalances2() [from: ${fromDate}, to: ${toDate}]`, err);
         return false;
     }
 }
