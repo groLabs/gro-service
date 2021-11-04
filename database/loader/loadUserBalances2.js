6c220368ebd64f8c7a37b6e7cf86083c290142ca
Sergi Juanati <wint3rmute@gmail.com>
2021-11-04 17:52:56 +0100

    Added subfunctions & comments on loadBalances2

--- database/loader/loadUserBalances2.js
+++ database/loader/loadUserBalances2.js
@@ -48,7 +48,17 @@ let lpCrvPwrd = [];
 let lpGroWeth = [];
 
 
-const getBalancesSC = async (users, block, offset, gvt, pwrd, gro, lpGroGvt, lpGroUsdc, lpCrvPwrd, lpGroWeth) => {
+/// @notice Retrieve user balances in a recursive way by batches
+/// @dev    - The amount of users per batch is defined in constant <BATCH>
+///         - The offset & batch will define the amount of users to be processed on
+///           every function iteration
+/// @param  users The array of users to be processed
+/// @param  block The block number to retrieve user balances at
+/// @param  offset The offset to track the start and end position in the users array
+///         to be processed on each iteration
+/// @return An array with 7 fixed subarrays, each of them containing all balances per token
+///         (gvt, pwrd, gro, gro/gvt, gro/usdc, 3crv, gro/weth)  
+const getBalancesSC = async (users, block, offset) => {
     try {
 
         const newOffset = (offset + BATCH >= users.length)
@@ -108,7 +118,7 @@ const getBalancesSC = async (users, block, offset, gvt, pwrd, gro, lpGroGvt, lpG
 
         return (newOffset >= users.length)
             ? [gvt, pwrd, gro, lpGroGvt, lpGroUsdc, lpCrvPwrd, lpGroWeth]
-            : getBalancesSC(users, block, newOffset, gvt, pwrd, gro, lpGroGvt, lpGroUsdc, lpCrvPwrd, lpGroWeth);
+            : getBalancesSC(users, block, newOffset);
 
     } catch (err) {
         handleErr(`loadUserBalances2->getBalancesSC()`, err);
@@ -153,7 +163,7 @@ const loadStakedBalance = (account, i, day, addr) => {
         } else {
             rowExcludedStaked++;
         }
-        
+
         resolve(true);
     });
 }
@@ -266,8 +276,97 @@ const cleanseVars = (scope) => {
     rowExcludedPooled = 0;
 }
 
+/// @notice Check time format (if defined) and return hours, minutes & seconds
+/// @dev    If time is not defined, return 23:59:59 by default
+/// @param  time The target time to load balances [format: HH:mm:ss]
+/// @return An array with 7 fixed positions: hours, minuts & seconds
+const checkTime = (time) => {
+    const isTimeOK = moment(time, 'HH:mm:ss', true).isValid();
+    if (!time) {
+        return [23, 59, 59];
+    } else if (isTimeOK) {
+        const hours = parseInt(time.substring(0, 2));
+        const minutes = parseInt(time.substring(3, 5));
+        const seconds = parseInt(time.substring(6, 8));
+        return [hours, minutes, seconds];
+    } else {
+        handleErr(`loadUserBalances2->checkTime(): invalid time format`, time);
+        return [-1, -1, -1];
+    }
+}
 
-/// @notice Load balances into USER_STD_FACT_BALANCES
+/// @notice Retrieve all distinct users that did any transfer (deposit, withdrawal, transer)
+///         from the beginning of the protocol
+/// @param  account The user address for cache query
+/// @return An array with all users to be processed
+const retrieveUsers = async (account) => {
+    let res;
+    if (account) {
+        res = {
+            rowCount: 1,
+            rows: [{
+                user_address: account
+            }],
+        }
+    } else {
+        // TODO: select distinct transfers where day <= target date (for future data reloads)
+        res = await query('select_distinct_users_transfers.sql', []);
+        if (res.status === QUERY_ERROR)
+            return [];
+    }
+    // Extract value from array of object [user_address: 0x...]
+    const users = res.rows.map(key => key.user_address);
+    return users;
+}
+
+/// @notice Check if target date >= deployment date of TokenCounter SC
+/// @param  day The target day [format: DD/MM/YYYY]
+/// @return True if target >= TokenCounter deployment date; False otherwise
+const checkTokenCounterDate = (day) => {
+    // TODO: replace by config/registry data (this is mainnet deployment)
+    const tokenCounterStartDate = moment.utc('26/10/2021', 'DD/MM/YYYY')
+        .add(10, 'hours')
+        .add(55, 'minutes')
+        .add(42, 'seconds');
+
+    if (!day.isSameOrAfter(tokenCounterStartDate)) {
+        const msg = `target date [${day}] before TokenCounter date [${tokenCounterStartDate}]`;
+        logger.error(`loadUserBalances2->loadUserBalances2(): ${msg}`);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/// @notice Show message logs after successful loads
+const showMsg = (account, date, rowCount, rowExcluded, table) => {
+    let msg3 = `**DB${account ? ' CACHE' : ''}: ${rowCount} record${isPlural(rowCount)} `;
+    msg3 += `added into ${table} `;
+    msg3 += (rowExcluded !== 0) ? `(excluded ${rowExcluded} with 0-balance) ` : '';
+    msg3 += `for date ${moment(date).format('DD/MM/YYYY')}`;
+    logger.info(msg3);
+}
+
+/// @notice Update table SYS_USER_LOADS with the last loads
+const updateSysTable = async (account, fromDate, toDate) => {
+    if (account) {
+        return true;
+    } else {
+        const [
+            resUnStaked,
+            resStaked,
+            resPooled
+        ] = await Promise.all([
+            loadTableUpdates('USER_STD_FACT_BALANCES_UNSTAKED', fromDate, toDate),
+            loadTableUpdates('USER_STD_FACT_BALANCES_STAKED', fromDate, toDate),
+            loadTableUpdates('USER_STD_FACT_BALANCES_POOLED', fromDate, toDate)
+        ]);
+        return (resUnStaked && resStaked && resPooled) ? true : false;
+    }
+}
+
+
+/// @notice Load user balances into USER_STD_FACT_BALANCES* tables
 /// @dev    - Data is sourced from SC calls to users' balances at a certain block number
 ///         according to the dates provided
 ///         - TokenCounter SC only available in mainnet from 26/10/2021
@@ -282,49 +381,18 @@ const loadUserBalances2 = async (
     time,
 ) => {
     try {
-        let hours, minutes, seconds;
-        // TODO: replace by config/registry data (this is mainnet deployment)
-        const tokenCounterStartDate = moment.utc('26/10/2021', 'DD/MM/YYYY')
-            .add(10, 'hours')
-            .add(55, 'minutes')
-            .add(42, 'seconds');
 
-        const isTimeOK = moment(time, 'HH:mm:ss', true).isValid();
-        if (!time) {
-            hours = 23;
-            minutes = 59;
-            seconds = 59;
-        } else if (isTimeOK) {
-            hours = parseInt(time.substring(0, 2));
-            minutes = parseInt(time.substring(3, 5));
-            seconds = parseInt(time.substring(6, 8));
-        } else {
-            handleErr(`loadUserBalances2->loadUserBalances2(): invalid time format`, time);
+        // Retrieve target time to load balances (23:59:59 by default)
+        const [hours, minutes, seconds] = checkTime(time);
+        if (hours === -1)
             return false;
-        }
 
-        // Get all distinct users with any transfer
-        let res;
-        let users;
-        if (account) {
-            res = {
-                rowCount: 1,
-                rows: [{
-                    user_address: account
-                }],
-            }
-        } else {
-            // TODO: select distinct transfers where day <= target date (for future data reloads)
-            res = await query('select_distinct_users_transfers.sql', []);
-            if (res.status === QUERY_ERROR)
-                return false;
-        }
+        // Retrieve users to load balances for
+        const users = await retrieveUsers(account);
 
-        // Extract value from array of object [user_address: 0x...]
-        users = res.rows.map(key => key.user_address);
-
-        // For each date, check each gvt & pwrd balance and insert data into USER_STD_FACT_BALANCES*
+        // Generate range of dates to be processed (in case fromDate <> toDate)
         const dates = generateDateRange(fromDate, toDate);
+
         logger.info(`**DB${account ? ' CACHE' : ''}: Processing ${users.length} user balance${isPlural(users.length)}...`);
 
         for (const date of dates) {
@@ -334,12 +402,10 @@ const loadUserBalances2 = async (
                 .add(minutes, 'minutes')
                 .add(seconds, 'seconds');
 
-            // Check if day >= deployment date of TokenCounter SC
-            if (!day.isSameOrAfter(tokenCounterStartDate)) {
-                const msg = `target date [${day}] before TokenCounter date [${tokenCounterStartDate}]`;
-                logger.error(`loadUserBalances2->loadUserBalances2(): ${msg}`);
+            // Check if target day >= deployment date of TokenCounter SC
+            const isTokenCounterValidDate = checkTokenCounterDate(day);
+            if (!isTokenCounterValidDate)
                 return false;
-            }
 
             const block = (await findBlockByDate(day, false)).block;
 
@@ -351,7 +417,7 @@ const loadUserBalances2 = async (
                 lpGroUsdc,
                 lpCrvPwrd,
                 lpGroWeth
-            ] = await getBalancesSC(users, block, 0, [], [], [], [], [], [], []);
+            ] = await getBalancesSC(users, block, 0);
 
             for (let i = 0; i < users.length; i++) {
 
@@ -381,33 +447,13 @@ const loadUserBalances2 = async (
         cleanseVars('all');
 
         // Update table SYS_USER_LOADS with the last loads
-        if (account) {
-            return true;
-        } else {
-            const [
-                resUnStaked,
-                resStaked,
-                resPooled
-            ] = await Promise.all([
-                loadTableUpdates('USER_STD_FACT_BALANCES_UNSTAKED', fromDate, toDate),
-                loadTableUpdates('USER_STD_FACT_BALANCES_STAKED', fromDate, toDate),
-                loadTableUpdates('USER_STD_FACT_BALANCES_POOLED', fromDate, toDate)
-            ]);
-            return (resUnStaked && resStaked && resPooled) ? true : false;
-        }
+        const finalResult = await updateSysTable(account, fromDate, toDate);
+        return finalResult;
 
     } catch (err) {
         handleErr(`loadUserBalances2->loadUserBalances2() [from: ${fromDate}, to: ${toDate}]`, err);
         return false;
     }
-}
-
-const showMsg = (account, date, rowCount, rowExcluded, table) => {
-    let msg3 = `**DB${account ? ' CACHE' : ''}: ${rowCount} record${isPlural(rowCount)} `;
-    msg3 += `added into ${table} `;
-    msg3 += (rowExcluded !== 0) ? `(excluded ${rowExcluded} with 0-balance) ` : '';
-    msg3 += `for date ${moment(date).format('DD/MM/YYYY')}`;
-    logger.info(msg3);
 }
 
 module.exports = {
