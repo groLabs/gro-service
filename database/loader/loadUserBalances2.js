20ba2c637d7b1af57cce740f3918aced17f8546a
Sergi Juanati <wint3rmute@gmail.com>
2021-11-04 12:47:01 +0100

    Inserts for user balances in parallel

--- database/loader/loadUserBalances2.js
+++ database/loader/loadUserBalances2.js
@@ -24,14 +24,28 @@ const {
     getBalancesCrvLP,
 } = require('../common/balanceUtil');
 const { BALANCES_BATCH: BATCH } = require('../constants');
-
-
 const { getConfig } = require('../../common/configUtil');
 const GRO_ADDRESS = getConfig('staker_pools.contracts.gro_address');
+
 const GRO_GVT_ADDRESS = getConfig('staker_pools.contracts.uniswap_gro_gvt_pool_address');
 const GRO_USDC_ADDRESS = getConfig('staker_pools.contracts.uniswap_gro_usdc_pool_address');
 const CRV_PWRD_ADDRESS = getConfig('staker_pools.contracts.curve_pwrd3crv_pool_address');
 const GRO_WETH_ADDRESS = getConfig('staker_pools.contracts.balancer_gro_weth_pool_address');
+
+let rowCountStaked = 0;
+let rowExcludedStaked = 0;
+let rowCountUnstaked = 0;
+let rowExcludedUnstaked = 0;
+let rowCountPooled = 0;
+let rowExcludedPooled = 0;
+
+let gvt = [];
+let pwrd = [];
+let gro = [];
+let lpGroGvt = [];
+let lpGroUsdc = [];
+let lpCrvPwrd = [];
+let lpGroWeth = [];
 
 
 const getBalancesSC = async (users, block, offset, gvt, pwrd, gro, lpGroGvt, lpGroUsdc, lpCrvPwrd, lpGroWeth) => {
@@ -101,6 +115,157 @@ const getBalancesSC = async (users, block, offset, gvt, pwrd, gro, lpGroGvt, lpG
         // return [] ?
     }
 }
+
+const loadStakedBalance = (account, i, day, addr) => {
+    return new Promise(async (resolve) => {
+
+        const isStakedBalance = (
+            gro[1].amount_staked[i]
+            + lpGroGvt[1].amount_staked_lp[i]
+            + lpGroUsdc[1].amount_staked_lp[i]
+            + gvt[1].amount_staked[i]
+            + lpCrvPwrd[1].amount_staked_lp[i]
+            + lpGroWeth[1].amount_staked_lp[i]
+            > 0)
+            ? true
+            : false;
+
+        const stakedParams = [
+            day,
+            getNetworkId(),
+            addr,
+            gro[1].amount_staked[i],            // pool0_staked_amount
+            lpGroGvt[1].amount_staked_lp[i],    // pool1_staked_amount
+            lpGroUsdc[1].amount_staked_lp[i],   // pool2_staked_amount
+            gvt[1].amount_staked[i],            // pool3_staked_amount
+            lpCrvPwrd[1].amount_staked_lp[i],   // pool4_staked_amount
+            lpGroWeth[1].amount_staked_lp[i],   // pool5_staked_amount
+            moment.utc(),
+        ];
+
+        if (isStakedBalance) {
+            // TODO: add cache query
+            const q = 'insert_user_std_fact_balances_staked.sql';
+            const result = await query(q, stakedParams);
+            if (result.status === QUERY_ERROR)
+                resolve(false);
+            rowCountStaked += result.rowCount;
+        } else {
+            rowExcludedStaked++;
+        }
+        
+        resolve(true);
+    });
+}
+
+const loadUnstakedBalance = (account, i, day, addr) => {
+    return new Promise(async (resolve) => {
+
+        const isUnstakedBalance = (
+            gvt[0].amount_unstaked[i]
+            + pwrd[0].amount_unstaked[i]
+            + gro[0].amount_unstaked[i]
+            > 0)
+            ? true
+            : false;
+
+        const unstakedParams = [
+            day,
+            getNetworkId(),
+            addr,
+            gvt[0].amount_unstaked[i],     // unstaked gvt amount
+            pwrd[0].amount_unstaked[i],    // unstaked pwrd amount
+            gro[0].amount_unstaked[i],     // unstaked gro amount
+            moment.utc(),
+        ];
+
+        if (isUnstakedBalance) {
+            const q = (account)
+                // ? 'insert_user_cache_fact_balances.sql'  // **** TODO ****
+                ? 'insert_user_std_fact_balances_unstaked.sql'  // **** for TESTING ONLY *****
+                : 'insert_user_std_fact_balances_unstaked.sql';
+            const result = await query(q, unstakedParams);
+            if (result.status === QUERY_ERROR)
+                resolve(false);
+            rowCountUnstaked += result.rowCount;
+        } else {
+            rowExcludedUnstaked++;
+        }
+
+        resolve(true);
+    });
+}
+
+const loadPooledBalance = (account, i, day, addr) => {
+    return new Promise(async (resolve) => {
+
+        const isPooledBalance = (
+            lpGroGvt[0].amount_unstaked_lp[i] +
+            lpGroGvt[2].lp_position[i][0] +
+            lpGroGvt[2].lp_position[i][1] +
+            lpGroUsdc[0].amount_unstaked_lp[i] +
+            lpGroUsdc[2].lp_position[i][0] +
+            lpGroUsdc[2].lp_position[i][1] +
+            lpCrvPwrd[0].amount_unstaked_lp[i] +
+            lpCrvPwrd[2].lp_position[i] +
+            lpGroWeth[0].amount_unstaked_lp[i] +
+            lpGroWeth[2].lp_position[i][0] +
+            lpGroWeth[2].lp_position[i][1]
+            > 0)
+            ? true
+            : false;
+
+        const pooledParams = [
+            day,
+            getNetworkId(),
+            addr,
+            lpGroGvt[0].amount_unstaked_lp[i],  // pool1_lp_amount
+            lpGroGvt[2].lp_position[i][0],      // pool1_gro_amount
+            lpGroGvt[2].lp_position[i][1],      // pool1_gvt_amount
+            lpGroUsdc[0].amount_unstaked_lp[i], // pool2_lp_amount
+            lpGroUsdc[2].lp_position[i][0],     // pool2_gro_amount
+            lpGroUsdc[2].lp_position[i][1],     // pool2_usdc_amount
+            lpCrvPwrd[0].amount_unstaked_lp[i], // pool4_lp_amount
+            lpCrvPwrd[2].lp_position[i],        // poll4_pwrd_amount
+            lpGroWeth[0].amount_unstaked_lp[i], // pool5_lp_amount
+            lpGroWeth[2].lp_position[i][0],     // pool5_gro_amount
+            lpGroWeth[2].lp_position[i][1],     // pool5_weth_amount
+            moment.utc(),
+        ];
+
+        if (isPooledBalance) {
+            // TODO: add cache query
+            const q = 'insert_user_std_fact_balances_pooled.sql';
+            const result = await query(q, pooledParams);
+            if (result.status === QUERY_ERROR)
+                resolve(false);
+            rowCountPooled += result.rowCount;
+        } else {
+            rowExcludedPooled++;
+        }
+
+        resolve(true);
+    });
+}
+
+const cleanseVars = (scope) => {
+    if (scope === 'all') {
+        gvt = [];
+        pwrd = [];
+        gro = [];
+        lpGroGvt = [];
+        lpGroUsdc = [];
+        lpCrvPwrd = [];
+        lpGroWeth = [];
+    }
+    rowCountStaked = 0;
+    rowExcludedStaked = 0;
+    rowCountUnstaked = 0;
+    rowExcludedUnstaked = 0;
+    rowCountPooled = 0;
+    rowExcludedPooled = 0;
+}
+
 
 /// @notice Load balances into USER_STD_FACT_BALANCES
 /// @dev    - Data is sourced from SC calls to users' balances at a certain block number
@@ -176,16 +341,9 @@ const loadUserBalances2 = async (
                 return false;
             }
 
-            let rowCountStaked = 0;
-            let rowExcludedStaked = 0;
-            let rowCountUnstaked = 0;
-            let rowExcludedUnstaked = 0;
-            let rowCountPooled = 0;
-            let rowExcludedPooled = 0;
-
             const block = (await findBlockByDate(day, false)).block;
 
-            const [
+            [
                 gvt,
                 pwrd,
                 gro,
@@ -199,125 +357,28 @@ const loadUserBalances2 = async (
 
                 const addr = users[i];
 
-                const isUnstakedBalance = (
-                    gvt[0].amount_unstaked[i]
-                    + pwrd[0].amount_unstaked[i]
-                    + gro[0].amount_unstaked[i]
-                    > 0)
-                    ? true
-                    : false;
+                const [
+                    resUnstaked,
+                    resStaked,
+                    resPooled,
+                ] = await Promise.all([
+                    loadUnstakedBalance(account, i, day, addr),
+                    loadStakedBalance(account, i, day, addr),
+                    loadPooledBalance(account, i, day, addr)
+                ]);
 
-                const isStakedBalance = (
-                    gro[1].amount_staked[i]
-                    + lpGroGvt[1].amount_staked_lp[i]
-                    + lpGroUsdc[1].amount_staked_lp[i]
-                    + gvt[1].amount_staked[i]
-                    + lpCrvPwrd[1].amount_staked_lp[i]
-                    + lpGroWeth[1].amount_staked_lp[i]
-                    > 0)
-                    ? true
-                    : false;
+                if (!resUnstaked || !resStaked || !resPooled)
+                    return false;
 
-                const isPooledBalance = (
-                    lpGroGvt[0].amount_unstaked_lp[i] +
-                    lpGroGvt[2].lp_position[i][0] +
-                    lpGroGvt[2].lp_position[i][1] +
-                    lpGroUsdc[0].amount_unstaked_lp[i] +
-                    lpGroUsdc[2].lp_position[i][0] +
-                    lpGroUsdc[2].lp_position[i][1] +
-                    lpCrvPwrd[0].amount_unstaked_lp[i] +
-                    lpCrvPwrd[2].lp_position[i] +
-                    lpGroWeth[0].amount_unstaked_lp[i] +
-                    lpGroWeth[2].lp_position[i][0] +
-                    lpGroWeth[2].lp_position[i][1]
-                    > 0)
-                    ? true
-                    : false;
-
-                const unstakedParams = [
-                    day,
-                    getNetworkId(),
-                    addr,
-                    gvt[0].amount_unstaked[i],     // unstaked gvt amount
-                    pwrd[0].amount_unstaked[i],    // unstaked pwrd amount
-                    gro[0].amount_unstaked[i],     // unstaked gro amount
-                    moment.utc(),
-                ];
-
-                const stakedParams = [
-                    day,
-                    getNetworkId(),
-                    addr,
-                    gro[1].amount_staked[i],            // pool0_staked_amount
-                    lpGroGvt[1].amount_staked_lp[i],    // pool1_staked_amount
-                    lpGroUsdc[1].amount_staked_lp[i],   // pool2_staked_amount
-                    gvt[1].amount_staked[i],            // pool3_staked_amount
-                    lpCrvPwrd[1].amount_staked_lp[i],   // pool4_staked_amount
-                    lpGroWeth[1].amount_staked_lp[i],   // pool5_staked_amount
-                    moment.utc(),
-                ];
-
-                const pooledParams = [
-                    day,
-                    getNetworkId(),
-                    addr,
-                    lpGroGvt[0].amount_unstaked_lp[i],  // pool1_lp_amount
-                    lpGroGvt[2].lp_position[i][0],      // pool1_gro_amount
-                    lpGroGvt[2].lp_position[i][1],      // pool1_gvt_amount
-                    lpGroUsdc[0].amount_unstaked_lp[i], // pool2_lp_amount
-                    lpGroUsdc[2].lp_position[i][0],     // pool2_gro_amount
-                    lpGroUsdc[2].lp_position[i][1],     // pool2_usdc_amount
-                    lpCrvPwrd[0].amount_unstaked_lp[i], // pool4_lp_amount
-                    lpCrvPwrd[2].lp_position[i],        // poll4_pwrd_amount
-                    lpGroWeth[0].amount_unstaked_lp[i], // pool5_lp_amount
-                    lpGroWeth[2].lp_position[i][0],     // pool5_gro_amount
-                    lpGroWeth[2].lp_position[i][1],     // pool5_weth_amount
-                    moment.utc(),
-                ];
-
-                // Unstaked amounts
-                if (isUnstakedBalance) {
-                    const q = (account)
-                        // ? 'insert_user_cache_fact_balances.sql'  // **** TODO ****
-                        ? 'insert_user_std_fact_balances_unstaked.sql'  // **** for TESTING ONLY *****
-                        : 'insert_user_std_fact_balances_unstaked.sql';
-                    const result = await query(q, unstakedParams);
-                    if (result.status === QUERY_ERROR)
-                        return false;
-                    rowCountUnstaked += result.rowCount;
-                } else {
-                    rowExcludedUnstaked++;
-                }
-
-                // Staked amounts
-                if (isStakedBalance) {
-                    // TODO: add cache query
-                    const q = 'insert_user_std_fact_balances_staked.sql';
-                    const result = await query(q, stakedParams);
-                    if (result.status === QUERY_ERROR)
-                        return false;
-                    rowCountStaked += result.rowCount;
-                } else {
-                    rowExcludedStaked++;
-                }
-
-                // Pooled amounts
-                if (isPooledBalance) {
-                    // TODO: add cache query
-                    const q = 'insert_user_std_fact_balances_pooled.sql';
-                    const result = await query(q, pooledParams);
-                    if (result.status === QUERY_ERROR)
-                        return false;
-                    rowCountPooled += result.rowCount;
-                } else {
-                    rowExcludedPooled++;
-                }
             }
 
             showMsg(account, date, rowCountUnstaked, rowExcludedUnstaked, 'USER_STD_FACT_BALANCES_UNSTAKED');
             showMsg(account, date, rowCountStaked, rowExcludedStaked, 'USER_STD_FACT_BALANCES_STAKED');
             showMsg(account, date, rowCountPooled, rowExcludedPooled, 'USER_STD_FACT_BALANCES_POOLED');
+            cleanseVars('rows');
         }
+
+        cleanseVars('all');
 
         // Update table SYS_USER_LOADS with the last loads
         if (account) {
