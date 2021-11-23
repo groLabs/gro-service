16b95d2f519fc0cc88bd102bd3a44b808a2aa7c9
Sergi Juanati <wint3rmute@gmail.com>
2021-11-23 19:07:01 +0100

    Adding dist to .gitignore

--- dist/regular/index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const scheduler = require('./scheduler/regularScheduler');
-const { initAllContracts } = require('../dist/contract/allContracts');
-initAllContracts().then(() => {
-    scheduler.startRegularJobs();
-});
