2bd5416a0370b8da6d9a1ba7d67935a34d85f47a
lily-57blocks <72856624+lily-57blocks@users.noreply.github.com>
2022-01-13 14:15:11 +0800

    once load data from chain failed, load data from local file (#128)
    Co-authored-by: lily.hu <7158887-lilyhu2014@users.noreply.gitlab.com>

--- registry/registryLoader.ts
+++ registry/registryLoader.ts
@@ -1,4 +1,8 @@
-import { getLatestContracts, getContractsHistory as getHistory } from './registry';
+import fs from 'fs';
+import {
+    getLatestContracts,
+    getContractsHistory as getHistory,
+} from './registry';
 
 const botEnv = process.env.BOT_ENV.toLowerCase();
 // eslint-disable-next-line import/no-dynamic-require
@@ -9,9 +13,19 @@ let latestContractsAddressByAddress;
 let contractsHistory;
 
 async function loadContractInfoFromRegistry() {
-    latestContractsAddress = await getLatestContracts(false);
-    latestContractsAddressByAddress = await getLatestContracts(true);
-    contractsHistory = await getHistory();
+    try {
+        latestContractsAddress = await getLatestContracts(false);
+        latestContractsAddressByAddress = await getLatestContracts(true);
+        contractsHistory = await getHistory();
+    } catch (error) {
+        logger.error(error);
+        const localInfo = readLocalContractConfig();
+        latestContractsAddress = localInfo.latestContractsAddress;
+        latestContractsAddressByAddress =
+            localInfo.latestContractsAddressByAddress;
+        contractsHistory = localInfo.contractsHistory;
+    }
+
     logger.info(
         `latestContractsAddress: ${JSON.stringify(latestContractsAddress)}`
     );
@@ -33,6 +47,41 @@ function getLatestContractsAddressByAddress() {
 
 function getContractsHistory() {
     return contractsHistory;
+}
+
+function readLocalContractConfig() {
+    const configFileFolder = `${__dirname}/config`;
+    const filePath = `${configFileFolder}/${process.env.NODE_ENV}_contracts_history.json`;
+    const data = fs.readFileSync(filePath, { flag: 'a+' });
+    let content = data.toString();
+    if (content.length === 0) {
+        content = '{}';
+    }
+    const contractsHistory = JSON.parse(content);
+    logger.info(
+        `local ${
+            process.env.NODE_ENV
+        } contracts history : ${filePath}:\n${JSON.stringify(contractsHistory)}`
+    );
+
+    const contractNames = Object.keys(contractsHistory);
+    logger.info(`contractNames: ${JSON.stringify(contractNames)}`);
+    const latestContractsAddress = {};
+    const latestContractsAddressByAddress = {};
+    for (let i = 0; i < contractNames.length; i += 1) {
+        const contractName = contractNames[i];
+        const contractHistory = contractsHistory[contractName];
+        const latestContractInfo = contractHistory[contractHistory.length - 1];
+        const contractAddress = latestContractInfo.address;
+        latestContractsAddress[contractName] = latestContractInfo;
+        latestContractsAddressByAddress[contractAddress] = latestContractInfo;
+    }
+
+    return {
+        latestContractsAddress,
+        latestContractsAddressByAddress,
+        contractsHistory,
+    };
 }
 
 export {
