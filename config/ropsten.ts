53f66645c5f392fa304815e35a47db018aa9608b
lily.hu <7158887-lilyhu2014@users.noreply.gitlab.com>
2022-01-13 16:39:20 +0800

    move bouncer proof file to env

--- config/ropsten.ts
+++ config/ropsten.ts
@@ -21,7 +21,7 @@ export const airdrop = {
 };
 export const gro_gate = {
     folder: '../grogate',
-    files: ['bouncer-10-proofs.json'],
+    files: [process.env.AVAX_BOUNCER_PROOF_FILE],
 };
 
 export const buoy_start_block = 10633347;
