92b8cb62a2db788a9575f7141894bf33336e6eaf
Sergi Juanati <wint3rmute@gmail.com>
2022-01-26 11:50:53 +0100

    ETH approvals in parser

--- database/parser/personalStatsApprovalsParser.ts
+++ database/parser/personalStatsApprovalsParser.ts
@@ -8,23 +8,111 @@ import {
     getBuoy,
 } from '../common/contractUtil';
 import {
+    errorObj,
     getNetwork,
     parseAmount
 } from '../common/globalUtil';
 import { getStableCoinIndex } from '../common/personalUtil';
 import {
     Base,
-    GlobalNetwork
+    TokenId,
+    Transfer,
+    GlobalNetwork,
+    ContractVersion,
 } from '../types';
 import {
     showInfo,
     showError,
     showWarning,
 } from '../handler/logHandler';
+import {
+    MAX_NUMBER,
+    QUERY_ERROR,
+    QUERY_SUCCESS
+} from '../constants';
+import { ICall } from '../interfaces/ICall';
 
-// const botEnv = process.env.BOT_ENV.toLowerCase();
-// const logger = require(`../../${botEnv}/${botEnv}Logger`);
 
+const getGvtValue = async (amount) => {
+    if (parseAmount(amount, Base.D18) > MAX_NUMBER) {
+        return -1;
+    } else {
+        const usdAmount = await getGroVault()
+            .getShareAssets(amount)
+            .catch((err) => {
+                showError('personalStatsApprovalsParser.ts->getGvtValue():', err);
+                return 0;
+            });
+        return parseAmount(usdAmount, Base.D18);
+    }
+}
+
+const personalStatsApprovalsParser = async (
+    contractVersion: ContractVersion,
+    globalNetwork: GlobalNetwork,
+    tokenId: TokenId,
+    logs: any,
+    side: Transfer,
+    account: string,
+): Promise<ICall> => {
+    try {
+
+        const approvals = [];
+        for (const log of logs) {
+
+            let coinAmount = ([
+                TokenId.DAI,
+                TokenId.GRO,
+                TokenId.GVT,
+                TokenId.groDAI_e]
+                .includes(tokenId))
+                ? parseAmount(log.args[2], Base.D18)
+                : parseAmount(log.args[2], Base.D6);
+
+            // If amount is above DB's NUMERIC(20,8) type, set to -1 (to save DB space),
+            // and convert to MAX_NUMBER within the personalStats handler
+            coinAmount = coinAmount < MAX_NUMBER
+                ? coinAmount
+                : -1;
+
+            // Exclude approval events with 0 amount
+            if (coinAmount !== 0)
+                approvals.push({
+                    block_number: log.blockNumber,
+                    tx_hash: log.transactionHash,
+                    network_id: getNetwork(globalNetwork).id,
+                    token_id: tokenId,
+                    version_id: contractVersion,
+                    sender_address: log.args[0],
+                    spender_address: log.args[1],
+                    coin_amount: coinAmount,
+                    coin_usd: tokenId === TokenId.GVT
+                        ? await getGvtValue(log.args[2])
+                        : coinAmount,
+                    creation_date: moment.utc(),
+                });
+        }
+
+        //console.log('here approvals:', approvals);
+
+        return {
+            status: QUERY_SUCCESS,
+            data: approvals,
+        };
+    } catch (err) {
+        const msg = `[side: ${side}]: ${err}`;
+        showError('personalStatsApprovalsParser.ts->personalStatsApprovalsParser()', msg);
+        return errorObj(msg);
+    }
+};
+
+export {
+    personalStatsApprovalsParser,
+};
+
+
+
+/*
 const getApprovalValue = async (tokenAddress, amount, tokenSymbol) => {
     try {
         let usdAmount = 0;
@@ -55,10 +143,16 @@ const getApprovalValue = async (tokenAddress, amount, tokenSymbol) => {
     }
 };
 
+*/
+
 //TODO: can't return false or approvals (not TS rulez)
-const parseApprovalEvents = async (
+/*
+const personalStatsApprovalsParser = async (
+    contractVersion: ContractVersion,
     globalNetwork: GlobalNetwork,
-    logs
+    logs: any,
+    side: Transfer,
+    account: string,
 ) => {
     try {
         const stableCoinInfo = await getStables();
@@ -98,7 +192,4 @@ const parseApprovalEvents = async (
         return false;
     }
 };
-
-export {
-    parseApprovalEvents,
-};
+*/
