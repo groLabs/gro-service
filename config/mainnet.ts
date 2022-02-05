2289a0a91ca42c90982c9bf4ab71aa8e8eeeb411
farhaan-ali <farhaan@gro.xyz>
2022-02-05 21:36:48 +0000

    Update stats generation paths

--- config/mainnet.ts
+++ config/mainnet.ts
@@ -112,10 +112,10 @@ export const transaction_long_pending = {
     rebalance: 60000,
 };
 export const keep_stats_file_number = 250;
-export const stats_folder = '../stats';
+export const stats_folder = '../stats/mainnet';
 export const log_folder = '../logs';
-export const blockNumberFile = '../stats/lastBlockNumber.json';
-export const stats_latest = '../stats/gro-latest.json';
+export const blockNumberFile = '../stats/mainnet/lastBlockNumber.json';
+export const stats_latest = '../stats/mainnet/gro-latest.json';
 export const pendingTransactionFile = '../pendingTransaction.json';
 export const vault_name = [
     'DAI yVault',
