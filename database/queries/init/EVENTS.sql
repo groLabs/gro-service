cdc8bc58da2723509bded06e76f0ef5f8bfaea2f
Sergi Juanati <wint3rmute@gmail.com>
2022-03-29 00:28:10 +0200

    Changes on events tables

--- database/queries/init/EVENTS.sql
+++ database/queries/init/EVENTS.sql
@@ -1,90 +1,166 @@
-CREATE TABLE gro."EVENT_APPROVALS" (
-    "event_id" CHARACTER VARYING (256) NOT NULL,
+CREATE TABLE gro."EV_TRANSACTIONS" (
+    "block_number" INTEGER NOT NULL,
+    "network_id" INTEGER NOT NULL,
+    "tx_hash" CHARACTER VARYING (66) NOT NULL,
+    "uncled" BOOLEAN NOT NULL,
+    "block_hash" CHARACTER VARYING (66) NULL,
+    "transaction_id" CHARACTER VARYING (256) NULL,
+    CONSTRAINT "EV_TRANSACTIONS_pkey" PRIMARY KEY (
+        block_number,
+        network_id,
+        tx_hash,
+        uncled
+    ) NOT DEFERRABLE INITIALLY IMMEDIATE
+) WITH (OIDS = FALSE);
+
+ALTER TABLE gro."EV_TRANSACTIONS" OWNER to postgres;
+
+CREATE TABLE gro."EV_APPROVALS" (
+    "log_index" INTEGER NOT NULL,
+    "transaction_id" CHARACTER VARYING (256) NOT NULL,
+    "contract_address" CHARACTER VARYING (42) NOT NULL,
+    "log_name" CHARACTER VARYING (42) NULL,
     "owner" CHARACTER VARYING (42) NULL,
     "spender" CHARACTER VARYING (42) NULL,
     "value" NUMERIC (20, 8) NULL,
     "token_id" SMALLINT NULL,
-    "version_id" SMALLINT NULL,
     "creation_date" TIMESTAMP (6) NULL,
-    CONSTRAINT "EVENT_APPROVALS_pkey" PRIMARY KEY (event_id) NOT DEFERRABLE INITIALLY IMMEDIATE
+    CONSTRAINT "EV_APPROVALS_pkey" PRIMARY KEY (
+        log_index,
+        transaction_id,
+        contract_address
+    ) NOT DEFERRABLE INITIALLY IMMEDIATE
 ) WITH (OIDS = FALSE);
 
-ALTER TABLE gro."EVENT_APPROVALS" OWNER to postgres;
+ALTER TABLE gro."EV_APPROVALS" OWNER to postgres;
 
-CREATE TABLE gro."EVENT_CLAIMS" (
-    "event_id" CHARACTER VARYING (256) NOT NULL,
+CREATE TABLE gro."EV_CLAIMS" (
+    "log_index" INTEGER NOT NULL,
+    "transaction_id" CHARACTER VARYING (256) NOT NULL,
+    "contract_address" CHARACTER VARYING (42) NOT NULL,
+    "log_name" CHARACTER VARYING (42) NULL,
     "from" CHARACTER VARYING (42) NULL,
     "pid" SMALLINT NULL,
     "vest" BOOLEAN NULL,
     "tranche_id" NUMERIC (20, 8) NULL,
     "amount" NUMERIC (20, 8) NULL,
     "creation_date" TIMESTAMP (6) NULL,
-    CONSTRAINT "EVENT_CLAIMS_pkey" PRIMARY KEY (event_id) NOT DEFERRABLE INITIALLY IMMEDIATE
+    CONSTRAINT "EV_CLAIMS_pkey" PRIMARY KEY (
+        log_index,
+        transaction_id,
+        contract_address
+    ) NOT DEFERRABLE INITIALLY IMMEDIATE
 ) WITH (OIDS = FALSE);
 
-ALTER TABLE gro."EVENT_CLAIMS" OWNER to postgres;
+ALTER TABLE gro."EV_CLAIMS" OWNER to postgres;
 
-CREATE TABLE gro."EVENT_DEPOSITS" (
-    "event_id" CHARACTER VARYING (256) NOT NULL,
+CREATE TABLE gro."EV_MULTI_CLAIMS" (
+    "log_index" INTEGER NOT NULL,
+    "transaction_id" CHARACTER VARYING (256) NOT NULL,
+    "contract_address" CHARACTER VARYING (42) NOT NULL,
+    "log_name" CHARACTER VARYING (42) NULL,
+    "from" CHARACTER VARYING (42) NULL,
+    "pids" INTEGER [] NULL,
+    "vest" BOOLEAN NULL,
+    "amounts" NUMERIC (20, 8) [] NULL,
+    "creation_date" TIMESTAMP (6) NULL,
+    CONSTRAINT "EV_MULTI_CLAIMS_pkey" PRIMARY KEY (
+        log_index,
+        transaction_id,
+        contract_address
+    ) NOT DEFERRABLE INITIALLY IMMEDIATE
+) WITH (OIDS = FALSE);
+
+ALTER TABLE gro."EV_MULTI_CLAIMS" OWNER to postgres;
+
+CREATE TABLE gro."EV_DEPOSITS" (
+    "log_index" INTEGER NOT NULL,
+    "transaction_id" CHARACTER VARYING (256) NOT NULL,
+    "contract_address" CHARACTER VARYING (42) NOT NULL,
+    "log_name" CHARACTER VARYING (42) NULL,
     "from" CHARACTER VARYING (42) NULL,
     "referral" CHARACTER VARYING (42) NULL,
     "pid" SMALLINT NULL,
     "token_id" SMALLINT NULL,
-    "version_id" SMALLINT NULL,
     "allowance" NUMERIC (20, 8) NULL,
     "amount1" NUMERIC (20, 8) NULL,
     "amount2" NUMERIC (20, 8) NULL,
     "amount3" NUMERIC (20, 8) NULL,
     "value" NUMERIC (20, 8) NULL,
     "creation_date" TIMESTAMP (6) NULL,
-    CONSTRAINT "EVENT_DEPOSITS_pkey" PRIMARY KEY (event_id) NOT DEFERRABLE INITIALLY IMMEDIATE
-) WITH (OIDS = FALSE);
-
-ALTER TABLE gro."EVENT_DEPOSITS" OWNER to postgres;
-
-CREATE TABLE gro."EVENT_MASTER" (
-    "block_number" INTEGER NOT NULL,
-    "network_id" INTEGER NOT NULL,
-    "tx_hash" CHARACTER VARYING (66) NOT NULL,
-    "contract_address" CHARACTER VARYING (42) NOT NULL,
-    "log_name" CHARACTER VARYING (255) NOT NULL,
-    "event_id" CHARACTER VARYING (256) NULL,
-    CONSTRAINT "EVENT_MASTER_pkey" PRIMARY KEY (
-        block_number,
-        network_id,
-        tx_hash,
-        contract_address,
-        log_name
+    CONSTRAINT "EV_DEPOSITS_pkey" PRIMARY KEY (
+        log_index,
+        transaction_id,
+        contract_address
     ) NOT DEFERRABLE INITIALLY IMMEDIATE
 ) WITH (OIDS = FALSE);
 
-ALTER TABLE gro."EVENT_MASTER" OWNER to postgres;
+ALTER TABLE gro."EV_DEPOSITS" OWNER to postgres;
 
-CREATE TABLE gro."EVENT_MULTI_WITHDRAWALS" (
-    "event_id" CHARACTER VARYING (256) NOT NULL,
+CREATE TABLE gro."EV_TRANSFERS" (
+    "log_index" INTEGER NOT NULL,
+    "transaction_id" CHARACTER VARYING (256) NOT NULL,
+    "contract_address" CHARACTER VARYING (42) NOT NULL,
+    "log_name" CHARACTER VARYING (42) NULL,
     "from" CHARACTER VARYING (42) NULL,
-    "referral" CHARACTER VARYING (42) NULL,
+    "to" CHARACTER VARYING (42) NULL,
+    "token_id" SMALLINT NULL,
+    "value" NUMERIC (20, 8) NULL,
+    "creation_date" TIMESTAMP (6) NULL,
+    CONSTRAINT "EV_TRANSFERS_pkey" PRIMARY KEY (
+        log_index,
+        transaction_id,
+        contract_address
+    ) NOT DEFERRABLE INITIALLY IMMEDIATE
+) WITH (OIDS = FALSE);
+
+ALTER TABLE gro."EV_TRANSFERS" OWNER to postgres;
+
+CREATE TABLE gro."EV_WITHDRAWALS" (
+    "log_index" INTEGER NOT NULL,
+    "transaction_id" CHARACTER VARYING (256) NOT NULL,
+    "contract_address" CHARACTER VARYING (42) NOT NULL,
+    "log_name" CHARACTER VARYING (42) NULL,
+    "from" CHARACTER VARYING (42) NULL,
+    "pid" INTEGER NULL,
+    "amount1" NUMERIC (20, 8) NULL,
+    "amount2" NUMERIC (20, 8) NULL,
+    "amount3" NUMERIC (20, 8) NULL,
+    "value" NUMERIC (20, 8) NULL,
+    "referral" BOOLEAN NULL,
     "balanced" BOOLEAN NULL,
     "all" BOOLEAN NULL,
-    "pid" INTEGER [] NULL,
-    "deductUsd" NUMERIC (20, 8) [] NULL,
-    "returnUsd" NUMERIC (20, 8) NULL,
+    "deductUsd" NUMERIC (20, 8) NULL,
     "lpAmount" NUMERIC (20, 8) NULL,
-    "amounts" NUMERIC (20, 8) [] NULL,
+    "allowance" NUMERIC (20, 8) NULL,
+    "totalLoss" NUMERIC (20, 8) NULL,
+    "token_id" SMALLINT NULL,
     "creation_date" TIMESTAMP (6) NULL,
-    CONSTRAINT "EVENT_MULTI_WITHDRAWALS_pkey" PRIMARY KEY (event_id) NOT DEFERRABLE INITIALLY IMMEDIATE
+    CONSTRAINT "EV_WITHDRAWALS_pkey" PRIMARY KEY (
+        log_index,
+        transaction_id,
+        contract_address
+    ) NOT DEFERRABLE INITIALLY IMMEDIATE
 ) WITH (OIDS = FALSE);
 
-ALTER TABLE gro."EVENT_MULTI_WITHDRAWALS" OWNER to postgres;
+ALTER TABLE gro."EV_WITHDRAWALS" OWNER to postgres;
 
-CREATE TABLE gro."EVENT_MULTI_CLAIMS" (
-    "event_id" CHARACTER VARYING (256) NOT NULL,
+CREATE TABLE gro."EV_MULTI_WITHDRAWALS" (
+    "log_index" INTEGER NOT NULL,
+    "transaction_id" CHARACTER VARYING (256) NOT NULL,
+    "contract_address" CHARACTER VARYING (42) NOT NULL,
+    "log_name" CHARACTER VARYING (42) NULL,
     "from" CHARACTER VARYING (42) NULL,
     "pids" INTEGER [] NULL,
-    "vest" BOOLEAN NULL,
-    "amount" NUMERIC (20, 8) [] NULL,
+    "amounts" NUMERIC (20, 8) [] NULL,
+    "value" NUMERIC (20, 8) [] NULL,
+    "token_id" SMALLINT NULL,
     "creation_date" TIMESTAMP (6) NULL,
-    CONSTRAINT "EVENT_MULTI_CLAIMS_pkey" PRIMARY KEY (event_id) NOT DEFERRABLE INITIALLY IMMEDIATE
+    CONSTRAINT "EV_MULTI_WITHDRAWALS_pkey" PRIMARY KEY (
+        log_index,
+        transaction_id,
+        contract_address
+    ) NOT DEFERRABLE INITIALLY IMMEDIATE
 ) WITH (OIDS = FALSE);
 
-ALTER TABLE gro."EVENT_MULTI_CLAIMS" OWNER to postgres;
+ALTER TABLE gro."EV_MULTI_WITHDRAWALS" OWNER to postgres;
