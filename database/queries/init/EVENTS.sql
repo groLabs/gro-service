87f2d9940baf9820d91f8ef53de6a278d873000e
Sergi Juanati <wint3rmute@gmail.com>
2022-05-20 19:30:37 +0200

    Added harvest event tables for Avax

--- database/queries/init/EVENTS.sql
+++ database/queries/init/EVENTS.sql
@@ -268,6 +268,44 @@ CREATE TABLE gro."EV_LAB_AH_LATEST_STRATEGY" (
 
 ALTER TABLE gro."EV_LAB_AH_LATEST_STRATEGY" OWNER to postgres;
 
+CREATE TABLE gro."EV_LAB_STRATEGY_HARVEST" (
+    "transaction_id" CHARACTER VARYING (66) NOT NULL,
+    "log_index" INTEGER NOT NULL,
+    "contract_address" CHARACTER VARYING (42) NOT NULL,
+    "block_timestamp" INTEGER NULL,
+    "log_name" CHARACTER VARYING (100) NOT NULL,
+    "profit" NUMERIC (20, 8) NULL,
+    "loss" NUMERIC (20, 8) NULL,
+    "debt_payment" NUMERIC (20, 8) NULL,
+    "debt_outstanding" NUMERIC (20, 8) NULL,
+    "creation_date" TIMESTAMP (6) WITHOUT TIME ZONE DEFAULT NOW(),
+    CONSTRAINT "EV_LAB_STRATEGY_HARVEST_pkey" PRIMARY KEY (
+        "transaction_id",
+        "log_index",
+        "contract_address"
+    ) NOT DEFERRABLE INITIALLY IMMEDIATE
+) WITH (OIDS = FALSE);
+
+ALTER TABLE gro."EV_LAB_STRATEGY_HARVEST" OWNER to postgres;
+
+CREATE TABLE gro."EV_LAB_VAULT_HARVEST" (
+    "transaction_id" CHARACTER VARYING (66) NOT NULL,
+    "log_index" INTEGER NOT NULL,
+    "contract_address" CHARACTER VARYING (42) NOT NULL,
+    "block_timestamp" INTEGER NULL,
+    "log_name" CHARACTER VARYING (100) NOT NULL,
+    "loss" BOOLEAN NULL,
+    "change" NUMERIC (20, 8) NULL,
+    "creation_date" TIMESTAMP (6) WITHOUT TIME ZONE DEFAULT NOW(),
+    CONSTRAINT "EV_LAB_VAULT_HARVEST_pkey" PRIMARY KEY (
+        "transaction_id",
+        "log_index",
+        "contract_address"
+    ) NOT DEFERRABLE INITIALLY IMMEDIATE
+) WITH (OIDS = FALSE);
+
+ALTER TABLE gro."EV_LAB_VAULT_HARVEST" OWNER to postgres;
+
 -- GRO TABLES
 
 CREATE TABLE gro."EV_GRO_DEPOSITS" (
@@ -319,6 +357,45 @@ CREATE TABLE gro."EV_GRO_WITHDRAWALS" (
 ) WITH (OIDS = FALSE);
 
 ALTER TABLE gro."EV_GRO_WITHDRAWALS" OWNER to postgres;
+
+CREATE TABLE gro."EV_HODLER_CLAIMS" (
+    "transaction_id" CHARACTER VARYING (66) NOT NULL,
+    "log_index" INTEGER NOT NULL,
+    "contract_address" CHARACTER VARYING (42) NOT NULL,
+    "block_timestamp" INTEGER NULL,
+    "log_name" CHARACTER VARYING (100) NOT NULL,
+    "user" CHARACTER VARYING (42) NULL,
+    "vest" BOOLEAN NULL,
+    "amount" NUMERIC (24, 12) NULL,
+    "creation_date" TIMESTAMP (6) WITHOUT TIME ZONE DEFAULT NOW(),
+    CONSTRAINT "EV_HODLER_CLAIMS_pkey" PRIMARY KEY (
+        "transaction_id",
+        "log_index",
+        "contract_address"
+    ) NOT DEFERRABLE INITIALLY IMMEDIATE
+) WITH (OIDS = FALSE);
+
+ALTER TABLE gro."EV_HODLER_CLAIMS" OWNER to postgres;
+
+CREATE TABLE gro."EV_AIRDROP_CLAIMS" (
+    "transaction_id" CHARACTER VARYING (66) NOT NULL,
+    "log_index" INTEGER NOT NULL,
+    "contract_address" CHARACTER VARYING (42) NOT NULL,
+    "block_timestamp" INTEGER NULL,
+    "log_name" CHARACTER VARYING (100) NOT NULL,
+    "account" CHARACTER VARYING (42) NULL,
+    "vest" BOOLEAN NULL,
+    "tranche_id" NUMERIC (20, 8) NULL,
+    "amount" NUMERIC (20, 8) NULL,
+    "creation_date" TIMESTAMP (6) WITHOUT TIME ZONE DEFAULT NOW(),
+    CONSTRAINT "EV_AIRDROP_CLAIMS_pkey" PRIMARY KEY (
+        "transaction_id",
+        "log_index",
+        "contract_address"
+    ) NOT DEFERRABLE INITIALLY IMMEDIATE
+) WITH (OIDS = FALSE);
+
+ALTER TABLE gro."EV_AIRDROP_CLAIMS" OWNER to postgres;
 
 -- STAKER TABLES
 
