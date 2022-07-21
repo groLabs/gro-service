c8b366f910da2c9980de905534791b9a58ac6ba6
Sergi Juanati <wint3rmute@gmail.com>
2022-07-21 09:43:19 +0200

    Updates on G2 tables

--- database/queries/init/EVENTS_G2.sql
+++ database/queries/init/EVENTS_G2.sql
@@ -10,6 +10,7 @@ CREATE TABLE gro."EV_G2_ROUTER_DEPOSITS" (
     "amounts" NUMERIC (20, 8) [] NULL,
     "tranche" BOOLEAN NULL,
     "slippage" INTEGER NULL,
+    "shares" NUMERIC (20, 8) NULL,
     "calc_amount" NUMERIC (20, 8) NULL,
     "creation_date" TIMESTAMP (6) WITHOUT TIME ZONE DEFAULT NOW(),
     CONSTRAINT "EV_G2_ROUTER_DEPOSITS_pkey" PRIMARY KEY (
@@ -89,6 +90,24 @@ CREATE TABLE gro."EV_G2_TRANCHE_WITHDRAWALS" (
 
 ALTER TABLE gro."EV_G2_TRANCHE_WITHDRAWALS" OWNER to postgres;
 
+CREATE TABLE gro."EV_G2_TRANCHE_BALANCES" (
+    "transaction_id" CHARACTER VARYING (66) NOT NULL,
+    "log_index" INTEGER NOT NULL,
+    "contract_address" CHARACTER VARYING (42) NOT NULL,
+    "block_timestamp" INTEGER NULL,
+    "log_name" CHARACTER VARYING (100) NOT NULL,
+    "balances" NUMERIC(20,8)[2] NULL,
+    "utilisation" NUMERIC(20,8) NULL,
+    "creation_date" TIMESTAMP (6) WITHOUT TIME ZONE DEFAULT NOW(),
+    CONSTRAINT "EV_G2_TRANCHE_BALANCES_pkey" PRIMARY KEY (
+        "transaction_id",
+        "log_index",
+        "contract_address"
+    ) NOT DEFERRABLE INITIALLY IMMEDIATE
+) WITH (OIDS = FALSE);
+
+ALTER TABLE gro."EV_G2_TRANCHE_BALANCES" OWNER to postgres;
+
 CREATE TABLE gro."EV_G2_VAULT_DEPOSITS" (
     "transaction_id" CHARACTER VARYING (66) NOT NULL,
     "log_index" INTEGER NOT NULL,
@@ -136,6 +155,7 @@ CREATE TABLE gro."EV_G2_VAULT_STRATEGY_CHANGES" (
     "contract_address" CHARACTER VARYING (42) NOT NULL,
     "block_timestamp" INTEGER NULL,
     "log_name" CHARACTER VARYING (100) NOT NULL,
+    "strategy" CHARACTER VARYING (42) NULL,
     "total_gain" NUMERIC (20, 8) NULL,
     "total_loss" NUMERIC (20, 8) NULL,
     "total_debt" NUMERIC (20, 8) NULL,
@@ -148,3 +168,22 @@ CREATE TABLE gro."EV_G2_VAULT_STRATEGY_CHANGES" (
 ) WITH (OIDS = FALSE);
 
 ALTER TABLE gro."EV_G2_VAULT_STRATEGY_CHANGES" OWNER to postgres;
+
+CREATE TABLE gro."EV_G2_VAULT_STRATEGY_ADDED" (
+    "transaction_id" CHARACTER VARYING (66) NOT NULL,
+    "log_index" INTEGER NOT NULL,
+    "contract_address" CHARACTER VARYING (42) NOT NULL,
+    "block_timestamp" INTEGER NULL,
+    "log_name" CHARACTER VARYING (100) NOT NULL,
+    "strategy" CHARACTER VARYING (42) NULL,
+    "strategy_id" INTEGER NULL,
+    "pos" INTEGER NULL,
+    "creation_date" TIMESTAMP (6) WITHOUT TIME ZONE DEFAULT NOW(),
+    CONSTRAINT "EV_G2_VAULT_STRATEGY_ADDED_pkey" PRIMARY KEY (
+        "transaction_id",
+        "log_index",
+        "contract_address"
+    ) NOT DEFERRABLE INITIALLY IMMEDIATE
+) WITH (OIDS = FALSE);
+
+ALTER TABLE gro."EV_G2_VAULT_STRATEGY_ADDED" OWNER to postgres;
